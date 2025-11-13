9188048167fe849d469d0da2471ff584deed302e
Leo <leozinho17silvacosta@gmail.com>
2025-11-12 18:45:54 -0800

    complete about section

---
+++ about/about.script.js
@@ -0,0 +1,7 @@
+
+window.addEventListener('DOMContentLoaded', () => {
+    const button = document.getElementById('my-work-button')
+    button.addEventListener('click', () => {
+        window.location.href= '/my-work/my-work.html'
+    })
+})
