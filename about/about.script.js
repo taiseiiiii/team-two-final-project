c4a252c86d7957f4b609718d4fd2e49ce5f76c75
taiseiiiii <isemiya.0509@gmail.com>
2025-11-16 19:11:13 -0800

    fix all path

--- about/about.script.js
+++ about/about.script.js
@@ -1,7 +1,6 @@
-
-window.addEventListener('DOMContentLoaded', () => {
-    const button = document.getElementById('my-work-button')
-    button.addEventListener('click', () => {
-        window.location.href= '/my-work/my-work.html'
-    })
-})
+window.addEventListener("DOMContentLoaded", () => {
+  const button = document.getElementById("my-work-button");
+  button.addEventListener("click", () => {
+    window.location.href = "/team-two-final-project/my-work/my-work.html";
+  });
+});
